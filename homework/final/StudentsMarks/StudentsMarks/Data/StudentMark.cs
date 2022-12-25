91aed96318f58a13aa1e735ea29c473839cb9783
Alexey <alexdtat@gmail.com>
2022-12-26 02:49:30 +0300

    Students' marks addition implementation.

---
+++ homework/final/StudentsMarks/StudentsMarks/Data/StudentMark.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMarks;
+
+public class StudentMark
+{
+    public int StudentMarkId { get; set; }
+    public string Name { get; set; } = "";
+    public string Course { get; set; } = "";
+    [Range(0, 10, ErrorMessage = "Grade must be in range 0 - 10.")]
+    public int Mark { get; set; }
+}
